84c28155918b0b064ad49098ad64b7c61db280da
Guilherme <guilhermepolicarpio@gmail.com>
2022-12-16 19:18:17 -0300

    feat: implemented message screen if user does not have a hotel

--- src/components/Hotels/index.js
+++ src/components/Hotels/index.js
@@ -4,6 +4,7 @@ import { Title } from '../Title';
 import { Subtitle } from '../Subtitle';
 import useHotel from '../../hooks/api/useHotel';
 import HotelCard from './HotelCard';
+import Typography from '@material-ui/core/Typography';
 
 export default function Hotels() {
   const { hotels } = useHotel();
@@ -12,12 +13,18 @@ export default function Hotels() {
   return (
     <>
       <TitleSpacing>Escolha de hotel e quarto</TitleSpacing>
-      <Subtitle>Primeiro, escolha seu hotel</Subtitle>
-      <HotelsCardContainer>
-        {hotels ? (
-          hotels.map(hotel => <HotelCard key={hotel.id} {...hotel} selectedHotel={selectedHotel} setSelectedHotel={setSelectedHotel} />)
-        ) : (<></>)}
-      </HotelsCardContainer>
+      {hotels ? (<>
+        <Subtitle>Primeiro, escolha seu hotel</Subtitle>
+        <HotelsCardContainer>
+          {hotels ? (
+            hotels.map(hotel => <HotelCard key={hotel.id} {...hotel} selectedHotel={selectedHotel} setSelectedHotel={setSelectedHotel} />)
+          ) : (<>
+          </>)}
+        </HotelsCardContainer>
+      </>):
+        (<><MessageContainer>
+          <PaymentRequiredMessage variant="h6">Sua modalidade de ingresso não inclui hospedagem Prossiga para a escolha de atividades</PaymentRequiredMessage>
+        </MessageContainer> </>)}     
     </>
   );
 }
@@ -30,4 +37,29 @@ const HotelsCardContainer = styled.section`
   flex-wrap: wrap;
   margin-top: 1rem;
   display: flex;
+`;
+
+const PaymentRequiredMessage = styled(Typography)`
+  margin-bottom: 20px!important;
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+  color: #8E8E8E;
+  max-width: 411px;
+`;
+
+const MessageContainer = styled(Typography)`
+  
+  word-wrap: break-word;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
