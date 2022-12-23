3c70298931fb05aa4efb4651db2faec0bb6d5ee9
Nathalia <nathalia.gramst@gmail.com>
2022-12-23 17:20:56 -0300

    fix: treating warnings

--- src/components/Payment/FinishPayment.js
+++ src/components/Payment/FinishPayment.js
@@ -2,7 +2,6 @@ import * as useTicket from '../../hooks/api/useTicket';
 import { Section } from '../Dashboard/Section';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
-import Typography from '@material-ui/core/Typography';
 import PaymentForm from './PaymentForm';
 import PaymentSucces from './paymentSucces';
 
@@ -46,10 +45,8 @@ export default function FinishPayment() {
         <PaymentForm cardData={cardData} setCardData={setCardData} ticketId={ticketId} setPaymentSucces={setPaymentSucces}/>
       }
     </> :
-      <><MessageContainer>
-        <PaymentRequiredMessage variant="h6">Finalize a seleção do ingresso para realizar o pagamento.</PaymentRequiredMessage>
-      </MessageContainer> </>  
-    }    
+      <Section.Warning>Finalize a seleção do ingresso para realizar o pagamento.</Section.Warning>
+    }
   </Section>;
 };
 
@@ -81,29 +78,4 @@ const TicketContainer = styled.div`
         line-height: 18.75px;
         color: #454545;
     }
-`;
-
-const PaymentRequiredMessage = styled(Typography)`
-  margin-bottom: 20px!important;
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 23px;
-  text-align: center;
-  color: #8E8E8E;
-  max-width: 411px;
-`;
-
-const MessageContainer = styled(Typography)`
-  
-  word-wrap: break-word;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 `;
